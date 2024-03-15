5c3d26170e96a098349affd86434f92199fd1826
Arun <sangeetha@RRs-MacBook-Air.local>
2024-03-15 15:53:33 +0000

    graphql_resolver_lambda

---
+++ 1_New_project_Arun/react-shopping-cart/aws-cdk/cdk.out/asset.b19a6b643ff0cda12cf691098ecc719d71bf490e42d4350696431f591f5467f0/handler.js
@@ -0,0 +1,4 @@
+// lambda/handler.js (or index.js)
+exports.main = async function(event, context) {
+    // Your Lambda handler logic here
+  };
--- 1_New_project_Arun/react-shopping-cart/infrastructure/lib/Arun-stack.ts
+++ 1_New_project_Arun/react-shopping-cart/infrastructure/lib/Arun-stack.ts
@@ -2,6 +2,10 @@ import * as cdk from "aws-cdk-lib";
 import * as s3 from "aws-cdk-lib/aws-s3";
 import { Construct } from "constructs";
 import * as appsync from "aws-cdk-lib/aws-appsync";
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as iam from 'aws-cdk-lib/aws-iam';
+import { S3Client, GetObjectCommand, S3Client} from '@aws-sdk/client-s3';
+import { getSignedUrl} from '@aws-sdk/s3-request-presigner';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class ArunStack extends cdk.Stack {
@@ -25,5 +29,51 @@ export class ArunStack extends cdk.Stack {
       },
       xrayEnabled: true, // Enable AWS X-Ray for monitoring and performance insights
     });
+
+    //Define lambda function
+    const photoLambda = new lambda.Function(this, 'PhotoLambda', {
+      runtime: lambda.Runtime.NODEJS_20_X,
+      code: lambda.Code.fromAsset('/path/to/your/lambda/code'),
+      handler: 'index-handler'
+
+    });
+
+    //S3 bucket policy
+    const s3BucketPolicy = new iam.PolicyStatement({
+      actions: ['s3:GetObject', 's3:PutObject'],
+      resources: ['arn:aws:s3:::arunstack-photosbucket2ac9d1f0-rtbivhso7zsy']
+
+    });
+    photoLambda.addToRolePolicy(s3BucketPolicy);
+
+    //Attach lambda as data source for appsync
+
+    const photoLambdaDataSource = photoapi.addLambdaDataSource('lambdaDataSource', photoLambda);
+
+    photoLambdaDataSource.createResolver('getPhotoResolver', {
+      typeName: 'query',
+      fieldName: 'getPresignedUrl'
+    });
+
+    const s3Client = new S3Client({region: 'us-east-1'});
+
+    const handler = async (event: any): Promise<{url : string}> => {
+
+      const {fileName} = event.arguments;
+
+      const command = new GetObjectCommand({
+
+        Bucket: 'arn:aws:s3:::arunstack-photosbucket2ac9d1f0-rtbivhso7zsy',
+        Key: fileName,
+      });
+
+      try {
+        const url = await getSignedUrl(s3Client, command, { expiresIn: 3600 }); // URL expires in 1 hour
+        return { url }; // Adjust according to your GraphQL schema, if needed
+      } catch (err) {
+        console.error(err);
+        throw new Error("Unable to generate URL");
+      }
+    }
   }
 }
